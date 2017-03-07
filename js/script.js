f141eb1cd3b8ed3c294de1a9ed5ad2634dd30448
andyvilsack <andy.vilsack@gmail.com>
2017-03-07 16:31:30 -0500

    getting the easy setting done

--- js/script.js
+++ js/script.js
@@ -83,11 +83,37 @@ function reset() {
 
 function startMath(difficulty)
 {
-	
+	if (difficulty == "easy")
+	{
+		easyDif();
+	}
+	else if (difficulty == "medium")
+	{
+		medDif();
+	}
+	else if (difficulty == "hard")
+	{
+		hardDif();
+	}
 }
+var correctAnswers = [false,false,false,false,false,false,false,false,false,false];
+var numCorrect = [];
+var tenouttaten = false;
 function easyDif ()
 {
-
+	var i = 1;
+	var randomNum1
+	var randomNum2
+	var questionNum = "";
+	for (i=0;i<=9;i++)
+	{
+		randomNum1 = Math.floor(Math.random()*(10)+1);
+		randomNum2 = Math.floor(Math.random()*(10)+1);
+		questionNum = "question"+(i+1);
+		document.getElementById(questionNum).innerHTML = randomNum1 + " + " + randomNum2 + " =   " ;
+		correctAnswers[i]=(randomNum1+randomNum2);
+	}	
+	alert(correctAnswers);
 }
 function medDif()
 {
@@ -95,5 +121,38 @@ function medDif()
 }
 function hardDif()
 {
-	
+
+}
+function stopMath()
+{
+	answerNum = "";
+	counterCorrect = 0;
+	for (i=0;i<=9;i++)
+	{
+		answerNum="text"+(i+1);
+		if(correctAnswers[i] == document.getElementById(answerNum).value)
+		{
+			numCorrect[i] = true;
+		}
+		else
+		{
+			numCorrect[i] = false;
+		}
+	}
+	alert(numCorrect);
+	for (j=0;j<=9;j++)
+	{
+		if(numCorrect[i] == true)
+		{
+			counterCorrect++;
+		}
+	}
+	if (counterCorrect == 10)
+	{
+		document.getElementById("outputDiv").innerHTML = "Congradulations you've got 10/10 correct!"
+	}
+	else
+	{
+		document.getElementById("outputDiv").innerHTML = "You've got " + counterCorrect + " out of 10 correct";
+	}
 }
